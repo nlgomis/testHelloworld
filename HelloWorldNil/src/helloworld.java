c0b4899f9653382cb2a5670630dd73354b8e3368
iaw49320237 <iaw49320237@h01.informatica.escoladeltreball.org>
2020-10-06 19:49:18 +0200

    c2

--- HelloWorldNil/src/helloworld.java
+++ HelloWorldNil/src/helloworld.java
@@ -4,6 +4,16 @@ public class helloworld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		
+		
+		int i = 4;
+		
+		int j = 56;
+		
+		System.out.println(i + j);
+		
+		
+		
 	}
 
 }
